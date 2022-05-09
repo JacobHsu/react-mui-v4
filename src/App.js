e7c8cf9c4901b4ffb16a061fc421573799a44c2b
jacobhsu <jacob.hsu.tw@gmail.com>
2022-05-09 17:13:14 +0800

    https://v4.mui.com/components/tables/

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import EnhancedTable from './Pages/EnhancedTable';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <EnhancedTable/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
